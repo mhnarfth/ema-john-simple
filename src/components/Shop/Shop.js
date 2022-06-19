e64833374cb80d77e2d0250cc3b92337ec7d8e28
mhnarfth <mhnarfth@gmail.com>
2022-06-19 19:37:54 +0600

    add to local storage implemented

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
+import {addToDb} from '../../utilities/fakedb';
 import './Shop.css';
 
 
@@ -18,6 +19,7 @@ const Shop = () => {
     const handleAddToCart = (product) => {
         const newCart = [...cart, product];
         setCart (newCart);
+        addToDb(product.key);
      }
     return (
         <div className='shop-container'>
