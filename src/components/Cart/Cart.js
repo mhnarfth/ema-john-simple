43634bd3b7f8521442e4530c8793fb5c3ddb0280
mhnarfth <mhnarfth@gmail.com>
2022-06-19 19:20:44 +0600

    total calculated and cart updated

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,10 +2,22 @@ import React from 'react';
 import './Cart.css';
 
 const Cart = (props) => {
+    
+    const {cart} = props;
+
+    const total = cart.reduce(((previous, product) => previous + product.price), 0);
+    const shipping =total>0? 15: 0;
+    const tax = (total+ shipping) *0.1;
+    const grandTotal = total + shipping + tax;
     return (
         <div>
             <h2>Order Summary</h2>
-            <h2>Items: {props.cart.length}</h2>
+            <h2>Items: {cart.length}</h2>
+            <p>Total Price: {total.toFixed(2)} </p>
+            <p>Shipping: {shipping} </p>
+            <p>Tax: {tax.toFixed(2)} </p>
+            <p>Grand Total: {grandTotal.toFixed(2)} </p>
+
         </div>
     );
 };
