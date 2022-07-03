408a414dbf1f507a74b376b89088d6d29aef78f8
mhnarfth <mhnarfth@gmail.com>
2022-07-03 13:59:33 +0600

    cart-added-in-Order-Review-Component

--- src/components/OrderReview/OrderReview.js
+++ src/components/OrderReview/OrderReview.js
@@ -1,16 +1,18 @@
 import React from "react";
 import useCart from "../../hooks/useCart";
 import useProducts from "../../hooks/useProducts";
+import Cart from "../Cart/Cart";
 
 const OrderReview = () => {
   const [products] = useProducts();
   const [cart] = useCart(products);
 
   return (
-    <div>
-      <h1>{products.length}</h1>
-      <h2>{cart.length}</h2>
-      <h2>This is Order Review</h2>
+    <div className="shop-container">
+      <div className="product-container"></div>
+      <div className="cart-container">
+        <Cart cart={cart}></Cart>
+      </div>
     </div>
   );
 };
