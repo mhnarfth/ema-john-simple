fe45db2f35ae3b8c95fb731559cd869b67908b7f
mhnarfth <mhnarfth@gmail.com>
2022-07-03 15:29:44 +0600

    Order-review-ui-updated-and-DB-updated

--- src/components/OrderReview/OrderReview.js
+++ src/components/OrderReview/OrderReview.js
@@ -1,15 +1,30 @@
 import React from "react";
 import useCart from "../../hooks/useCart";
 import useProducts from "../../hooks/useProducts";
+import { deleteFromDb } from "../../utilities/fakedb";
 import Cart from "../Cart/Cart";
+import ReviewItem from "../ReviewItem/ReviewItem";
 
 const OrderReview = () => {
   const [products] = useProducts();
-  const [cart] = useCart(products);
+  const [cart, setCart] = useCart(products);
+
+  const handleRemove = (key) => {
+    const newCart = cart.filter((product) => product.key !== key);
+    setCart(newCart);
+    deleteFromDb(key);
+  };
 
   return (
     <div className="shop-container">
-      <div className="product-container"></div>
+      <div className="product-container">
+        {cart.map((product) => (
+          <ReviewItem
+            product={product}
+            handleRemove={handleRemove}
+          ></ReviewItem>
+        ))}
+      </div>
       <div className="cart-container">
         <Cart cart={cart}></Cart>
       </div>
---
+++ src/components/ReviewItem/ReviewItem.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const ReviewItem = (props) => {
+  const { key, name, price, quantity } = props.product;
+  const { handleRemove } = props;
+
+  return (
+    <div>
+      <h4>{name}</h4>
+      <p>Price: {price}</p>
+      <p>Quantity: {quantity}</p>
+      <button onClick={() => handleRemove(key)} className="btn-regular">
+        Remove
+      </button>
+    </div>
+  );
+};
+
+export default ReviewItem;
