63cc72f0c4e0770899920ae2906edd1e91057740
mhnarfth <mhnarfth@gmail.com>
2022-07-03 12:46:08 +0600

    React-router-added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-import Header from './components/Header/Header';
-import Shop from './components/Shop/Shop';
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/Header/Header";
+import Shop from "./components/Shop/Shop";
+import { Routes, Route, Link } from "react-router-dom";
+import OrderReview from "./components/OrderReview/OrderReview";
+import Inventory from "./components/Inventory/Inventory";
+import NotFound from "./components/NotFound/NotFound";
 
 function App() {
   return (
     <div>
-     <Header></Header>
-     <Shop></Shop>
+      <Header></Header>
+
+      <Routes>
+        <Route path="shop" element={<Shop />} />
+        <Route path="review" element={<OrderReview />} />
+        <Route path="inventory" element={<Inventory />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
